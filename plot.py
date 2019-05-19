9fb0ffb3a27df61f39a6012fc6da58afb16097f8
João Marcelo Tozato <jmttzt@Joaos-MacBook.local>
2019-05-19 15:30:36 -0300

    ajustes imgs

--- plot.py
+++ plot.py
@@ -11,9 +11,9 @@ plt.savefig('irisOriginal.pdf', bbox_inches= 'tight')
 # %matplotlib inline
 #cl_number,sepal_length,sepal_width,petal_length,petal_width,dist
 df = pd.read_csv('clusters.csv', usecols = ['cl_number','sepal_length','sepal_width','petal_length','petal_width'])
-df.cl_number[df.cl_number == 1] = 'versicolor'
+df.cl_number[df.cl_number == 3] = 'versicolor'
 df.cl_number[df.cl_number == 2] = 'setosa'
-df.cl_number[df.cl_number == 3] = 'virginica'
+df.cl_number[df.cl_number == 1] = 'virginica'
 # print(df.head())
 plt.figure()
 sns.pairplot(df, hue = "cl_number")
