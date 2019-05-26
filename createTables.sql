9e8959dbc222e642f9e5a03b416fd1550cd136e7
João Marcelo Tozato <jmttzt@Joaos-MacBook.local>
2019-05-26 17:41:49 -0300

    comentarios e testes

---
+++ createTables.sql
@@ -0,0 +1,57 @@
+CREATE TABLE IRIS(
+    sepal_length REAL,
+    sepal_width REAL,
+    petal_length REAL,
+    petal_width REAL,
+    species INTEGER
+);
+-- INSIRA O PATH PARA IRIS.DATA CONTIDA NO ARQUIVO COMPRIMIDO
+-- E IMPORTANTE UTILIZAR ESTE ARQUIVO, VISTO QUE HA MODIFICACOES FEITAS
+-- PREVIAMENTE A FIM DE QUE A COLUNA SPECIES SEJA UM NUMERO INTEIRO
+COPY IRIS FROM '/Users/jmttzt/Desktop/5Periodo/BD2/trabalho1/iris.data' DELIMITER ',';
+
+CREATE TABLE IRIS_NORMALIZADO(
+	cod SERIAL,
+    sepal_length REAL,
+    sepal_width REAL,
+    petal_length REAL,
+    petal_width REAL,
+    species INTEGER
+);
+
+
+CREATE TABLE CENTROID(
+	cl_number integer,
+	sepal_length REAL,
+    sepal_width REAL,
+    petal_length REAL,
+    petal_width REAL
+);
+
+
+CREATE TABLE DISTANCIAS(
+	cod integer,
+	cl_number integer,
+	sepal_length REAL,
+	sepal_width REAL, 
+	petal_length REAL,
+	petal_width REAL,
+	dist REAL
+);
+
+CREATE TABLE CLUSTERS(
+	cl_number integer,
+	sepal_length REAL,
+	sepal_width REAL, 
+	petal_length REAL,
+	petal_width REAL,
+	dist REAL
+);
+
+CREATE TABLE NEW_CENTROID(
+	cl_number integer,
+	sepal_length REAL,
+    sepal_width REAL,
+    petal_length REAL,
+    petal_width REAL
+);
--- plot.py
+++ plot.py
@@ -4,17 +4,28 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set(style="ticks", color_codes=True)
 
-irisOriginal = sns.load_dataset("iris")
-plt.figure()
-sns.pairplot(irisOriginal, hue="species")
-plt.savefig('irisOriginal.pdf', bbox_inches= 'tight')
+# irisOriginal = sns.load_dataset("iris")
+# plt.figure()
+# sns.pairplot(irisOriginal, hue="species")
+# plt.savefig('irisOriginal.pdf', bbox_inches= 'tight')
 # %matplotlib inline
 #cl_number,sepal_length,sepal_width,petal_length,petal_width,dist
 df = pd.read_csv('clusters.csv', usecols = ['cl_number','sepal_length','sepal_width','petal_length','petal_width'])
-df.cl_number[df.cl_number == 3] = 'versicolor'
-df.cl_number[df.cl_number == 2] = 'setosa'
-df.cl_number[df.cl_number == 1] = 'virginica'
+
+
+for i in range(len(df.cl_number.unique().tolist())):
+    df.cl_number[df.cl_number == i+1] = 'cluster: ' + str(i+1)
+    
+# df.cl_number[df.cl_number == 3] = 'versicolor'
+# df.cl_number[df.cl_number == 2] = 'setosa'
+# df.cl_number[df.cl_number == 1] = 'virginica'
+# df.cl_number[df.cl_number == 4] = 'cluster 4'
+# df.cl_number[df.cl_number == 5] = 'cluster 5'
+# df.cl_number[df.cl_number == 6] = 'cluster 6'
+#
+#talvez haja problemas com a legenda do grafico
+#testar setar a legenda manualmente
 # print(df.head())
 plt.figure()
 sns.pairplot(df, hue = "cl_number")
-plt.savefig('irisPLPGSQL.pdf', bbox_inches = 'tight')
+plt.savefig('irisPLPGSQL_100iteracoes_k=' + str(len(df.cl_number.unique().tolist())) + '.pdf', bbox_inches = 'tight')
